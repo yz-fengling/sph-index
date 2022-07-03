274a8d75d6a2d612e29f368d556a07fde5faac4f
风灵 <z2569126731@163.com>
2022-07-03 22:06:12 +0800

    feat: 新增了路由跳转功能

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
   <div id="app" class="outer">
     <!-- 头部 -->
     <Header></Header>
-    <!-- 底部 -->
-    <Footer></Footer>
+    <!-- 路由组件出口的位置:不设置路由组件不知道在哪里显示 -->
+    <router-view></router-view>
+    <!-- 底部，登录和注册页面不显示页脚，此属性定义在路由配置中 -->
+    <Footer v-show="$route.meta.show"></Footer>
   </div>
 </template>
 
 <script>
-import Header from './components/header/index'
-import Footer from './components/footer/index'
+import Header from './components/Header/index'
+import Footer from './components/Footer/index'
 export default {
   name: 'App',
   components: {
---
+++ src/pages/Search/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>query:{{$route.query.keyword}}</h1>
+    <h1>params:{{$route.params.keyword}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
